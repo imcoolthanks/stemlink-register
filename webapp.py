82f3e3163c3d8bbba30d46613fcf56f939047c73
imcoolthanks <wongq9999@outlook.com>
2022-08-25 11:37:49 +0800

    webapp changed

--- webapp.py
+++ webapp.py
@@ -1,6 +1,5 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
 
 if __name__ == "__main__":
         app.run()
