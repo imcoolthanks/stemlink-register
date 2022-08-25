c96cebf7696288b61154c8cf44f6bb89eb86a246
imcoolthanks <wongq9999@outlook.com>
2022-08-25 12:33:08 +0800

    webapp

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
 # Entry point for the application.
 from views import app
-from . import views    # For application discovery by the 'flask' command.
+import views    # For application discovery by the 'flask' command.
 
