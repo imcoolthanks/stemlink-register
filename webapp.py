a37a876d616189a182b2f0ef99a02a455fb47dca
imcoolthanks <wongq9999@outlook.com>
2022-08-25 11:39:45 +0800

    heroku

--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
+from views import app
+import views    # For application discovery by the 'flask' command.
 
 if __name__ == "__main__":
         app.run()
